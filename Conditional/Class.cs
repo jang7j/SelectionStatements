79730ad8dc57b8f7ec847fc52bc19ca0a396afbe
Yun Jang <jang7j@yahoo.com>
2022-08-10 14:12:26 -0500

    initial commit

---
+++ Conditional/Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionStatements;
+
+internal class Class
+{
+    public static void Guess(int userInput, int guess)
+    {
+        if (userInput < guess)
+        {           
+            Console.WriteLine("too low");
+        }
+
+        else if (userInput > guess)
+        {
+            Console.WriteLine("too high");
+        }              
+        else
+        {
+            Console.WriteLine("You guessed it!!!");
+        }
+        
+    }
+}
---
+++ Conditional/Program.cs
@@ -0,0 +1,11 @@
+﻿using SelectionStatements;
+
+var r = new Random();
+var guess = r.Next(1, 1000);
+
+Console.WriteLine("Guess the number: ");
+var userInput = int.Parse(Console.ReadLine());
+
+Class.Guess(userInput, guess);
+
+
